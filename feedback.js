c588cb2edf9844068e509a25245c963658239b68
DiGureev <dianagureev@gmail.com>
2024-07-31 22:18:00 +0300

    feedback

---
+++ feedback.js
@@ -0,0 +1,10 @@
+// Very nice project! I love the idea and the way you have organised your project! 
+// Thanks for using the neon db instead of a local db - it made it so much easier to check and made your project much stable.
+
+// I got an error when creating a profile: Error creating tables: error: drop table if exists ""fitbuddyapp"" - cannot drop table fitbuddyapp because other objects depend on it. This can happen if you have some foreign keys that depend on it.
+
+// As an improvement I would suggest logging in with regular info and then creating a profile with additional info. For example, if I log out and need to log in again, I have to add all the info once more? It is not that user friendly.
+
+// Don't forget to put node_modules and .env in .gitignore :) 
+
+// Overall very good work!
